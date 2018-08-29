9ab0f1a8d8c05208645389a7842902a969a187b5
justinazares <jzars.webdev@icloud.com>
2018-08-29 10:53:47 -0700

    add session to the state and add logout button

--- scratch-app-client/src/containers/Login.js
+++ scratch-app-client/src/containers/Login.js
@@ -31,7 +31,7 @@ export default class Login extends Component {
     // Login to Amazon Cognito
     // try {
     //   await Auth.signIn(this.state.email, this.state.password);
-    //   alert("Logged In")
+    //   this.props.userHasAuthenticated(true);
     // } catch (e) {
     //   alert(e.message)
     // }
